c25ae05d7f77c2507a20d017003ebf78d617649b
Jake Soenneker <jake@soenneker.com>
2025-02-05 14:56:21 -0600

    Cancellation

--- test/Soenneker.Validators.Email.Disposable.Online.Tests/EmailDisposableOnlineValidatorTests.cs
+++ test/Soenneker.Validators.Email.Disposable.Online.Tests/EmailDisposableOnlineValidatorTests.cs
@@ -4,7 +4,6 @@ using Soenneker.Tests.FixturedUnit;
 using Soenneker.Validators.Email.Disposable.Online.Abstract;
 using Xunit;
 
-
 namespace Soenneker.Validators.Email.Disposable.Online.Tests;
 
 [Collection("Collection")]
@@ -20,14 +19,14 @@ public class EmailDisposableOnlineValidatorTests : FixturedUnitTest
     [Fact]
     public async Task Validate_on_known_temporary_should_be_false()
     {
-        bool? result = await _validator.Validate("blah@10minutemail.com");
+        bool? result = await _validator.Validate("blah@10minutemail.com", CancellationToken);
         result.Should().BeFalse();
     }
 
     [Fact]
     public async Task Validate_on_known_google_should_be_true()
     {
-        bool? result = await _validator.Validate("blah@gmail.com");
+        bool? result = await _validator.Validate("blah@gmail.com", CancellationToken);
         result.Should().BeTrue();
     }
 }
